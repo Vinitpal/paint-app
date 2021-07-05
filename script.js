6d3f5bc490353c9b0ee7f4e607ee6f5f67335ae4
Vinit <vs361017@gmail.com>
2021-07-05 13:57:24 +0530

    updated ui

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let tool = "brush";
 let size = 5;
-let color = "white";
+// let color = "white";
 let mousePressed = false;
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 let x, y;
+let existingLines = [];
 
 const brush = document.getElementById("brush");
+const line = document.getElementById("line");
 const eraser = document.getElementById("eraser");
 const rectangle = document.getElementById("rectangle");
 const circle = document.getElementById("circle");
 const clear = document.getElementById("clear");
 const save = document.getElementById("save");
+
+const items = document.querySelectorAll(".items");
+
+let itemSelected = true;
+
+// ----- Pencil and toolbox Menu -----
+const brush_menu = document.getElementById("brush-menu");
+
+items.forEach((item, idx) => {
+  item.addEventListener("click", () => {
+    items.forEach((item) => item.classList.remove("active"));
+    if (items[idx].classList.contains("pencil")) {
+      brush.classList.add("active");
+      brush_menu.classList.add("pencil-active");
+    } else {
+      items[idx].classList.add("active");
+      brush_menu.classList.remove("pencil-active");
+    }
+  });
+});
 
 canvas.addEventListener("mousedown", brushDown);
 canvas.addEventListener("mousemove", brushMove);
@@ -21,17 +44,28 @@ canvas.addEventListener("mouseup", brushUp);
 
 // todo color + size change
 
+const options = document.querySelectorAll(".options-items div");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let color = option.className;
+    colorChange(color);
+  });
+});
+
 let restoreArray = [];
 let restoreIndex = -1;
 
-function colorChange() {
-  myColor = color.value;
-  ctx.strokeStyle = color;
+function colorChange(color) {
+  myColor = color;
+  ctx.strokeStyle = myColor;
 }
 
 function sizeChange(mySize) {
   ctx.lineWidth = mySize;
 }
+
+// ----- coordinates -----
 
 function getCoordinates(canvas, e) {
   let rect = canvas.getBoundingClientRect();
@@ -41,41 +75,115 @@ function getCoordinates(canvas, e) {
   };
 }
 
+// ----- draw functions -----
+
 function drawBrush(canvas, x, y) {
   if (mousePressed) {
+    ctx.globalCompositeOperation = "source-over";
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
 
+function drawLine(x, y) {
+  if (mousePressed) {
+    ctx.globalCompositeOperation = "source-over";
+    ctx.moveTo(x, y);
+    ctx.stroke();
+  }
+}
+
+// ----- brush function - down - move - up -----
+
 function brushDown(e) {
   mousePressed = true;
-  var coordinates = getCoordinates(canvas, e);
-  x0 = coordinates.x;
-  y0 = coordinates.y;
+  let coordinates = getCoordinates(canvas, e);
+  let x0 = coordinates.x;
+  let y0 = coordinates.y;
+
   ctx.beginPath();
-  ctx.moveTo(x0, y0);
-  ctx.lineTo(x0, y0);
-  ctx.stroke();
+  colorChange();
+
+  if (tool === "brush") {
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x0, y0);
+    ctx.stroke();
+  } else if (tool === "eraser") {
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.moveTo(x0, y0);
+    ctx.stroke();
+    ctx.fill();
+  } else if (tool === "line") {
+    existingLines = [
+      {
+        startX: x0,
+        startY: y0,
+      },
+    ];
+    drawLine(x0, y0);
+  }
+  // else if (tool === "circle") {
+  //   // ctx.arc(x0, y0, size, 0, Math.PI * 2);
+  //   // ctx.stroke;
+  // }
 }
 
 function brushMove(e) {
   let coordinates = getCoordinates(canvas, e);
-  x1 = coordinates.x;
-  y1 = coordinates.y;
-  drawBrush(canvas, x1, y1);
+  let x1 = coordinates.x;
+  let y1 = coordinates.y;
+
+  existingLines.push({
+    lastX: x1,
+    lastY: y1,
+  });
+
+  sizeChange(size);
+
+  if (tool === "brush") {
+    drawBrush(canvas, x1, y1);
+  } else if (tool === "eraser" && mousePressed) {
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 10;
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
+  } else if (tool === "line" && mousePressed) {
+    // clearCanvas();
+    let prevX = existingLines[1].lastX;
+    let prevY = existingLines[1].lastY;
+
+    // ctx.moveTo(prevX, prevY);
+    // ctx.lineTo(x1, y1);
+    // console.log("working2");
+    ctx.stroke();
+  }
+  // else if (tool === "circle") {
+  //   ctx.arc(x1, y1, 8, 0, Math.PI * 2);
+  //   ctx.stroke;
+  // }
 }
 
-function brushUp() {
+function brushUp(e) {
+  stop = getCoordinates(canvas, e);
+  stopX = stop.x;
+  stopY = stop.y;
   mousePressed = false;
 
+  if (tool === "line") {
+    ctx.lineTo(stopX, stopY);
+    ctx.stroke();
+  }
   if (!mousePressed) {
     restoreArray.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
     restoreIndex++;
   }
 }
 
+// ----- click handler functions -----
+
 function brushClick() {
+  tool = "brush";
   colorChange();
   sizeChange(size);
 
@@ -85,18 +193,38 @@ function brushClick() {
 }
 
 function eraserClick() {
-  ctx.strokeStyle = "#16161a";
-  sizeChange(size * 3);
+  tool = "eraser";
+  // ctx.strokeStyle = "#16161a";
+  // sizeChange(size * 3);
 
-  canvas.addEventListener("mousedown", brushDown, false);
-  canvas.addEventListener("mousemove", brushMove, false);
-  canvas.addEventListener("mouseup", brushUp, false);
+  canvas.addEventListener("mousedown", brushDown);
+  canvas.addEventListener("mousemove", brushMove);
+  canvas.addEventListener("mouseup", brushUp);
+}
+
+function lineClick() {
+  tool = "line";
+
+  canvas.addEventListener("mousedown", brushDown);
+  canvas.addEventListener("mousemove", brushMove);
+  canvas.addEventListener("mouseup", brushUp);
+}
+
+function circleClick() {
+  tool = "circle";
+
+  canvas.addEventListener("mousedown", brushDown);
+  canvas.addEventListener("mousemove", brushMove);
+  canvas.addEventListener("mouseup", brushUp);
 }
 
 brush.addEventListener("click", brushClick);
 eraser.addEventListener("click", eraserClick);
+line.addEventListener("click", lineClick);
+circle.addEventListener("click", circleClick);
 
-// ctrl + z aka undo functioning
+// ----- undo functioning -----
+
 document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.key === "z") {
     console.log("ctrl + z");
@@ -112,6 +240,32 @@ function undo() {
     restoreArray.pop();
     ctx.putImageData(restoreArray[restoreIndex], 0, 0);
   }
+}
+
+// ----- clear screen function -----
+clear.addEventListener("click", clearCanvas);
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  restoreIndex = -1;
+  restoreArray = [];
+}
+
+// ----- Save Button function -----
+// save.addEventListener("click", function () {
+//   let imageName = prompt("Please enter image name");
+//   let canvasDataURL = canvas.toDataURL();
+//   let a = document.createElement("a");
+//   a.href = canvasDataURL;
+//   a.download = imageName || none;
+//   a.click();
+// });
+
+// ----- window resize function -----
+resize();
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
 }
 
 // let restoreArray = [];
@@ -224,28 +378,3 @@ function undo() {
 //     ctx.putImageData(restoreArray[restoreIndex], 0, 0);
 //   }
 // }
-
-// clear screen function
-clear.addEventListener("click", clearCanvas);
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // restoreIndex = -1;
-  // restoreArray = [];
-}
-
-// Handle Save Button
-save.addEventListener("click", function () {
-  let imageName = prompt("Please enter image name");
-  let canvasDataURL = canvas.toDataURL();
-  let a = document.createElement("a");
-  a.href = canvasDataURL;
-  a.download = imageName || none;
-  a.click();
-});
-
-resize();
-function resize() {
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-}
